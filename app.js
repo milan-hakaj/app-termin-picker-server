ac562e6b0d43f92564160bf5f011da307d313b36
milanhakaj <milan.hakaj@computerrock.com>
2019-01-19 20:17:25 +0100

    mejson

--- app.js
+++ app.js
@@ -9,44 +9,44 @@ const routesUser = require('./api/routes/users');
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-
-  if(req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-  }
-  next();
-});
-
-app.use('/users', routesUser);
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true
-  }).then(() => console.log('mongoose ok'))
-    .catch(() => console.log('mongoose error'));
-
-app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  })
-});
+//
+// app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//
+//   if(req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//   }
+//   next();
+// });
+//
+// app.use('/users', routesUser);
+//
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true
+//   }).then(() => console.log('mongoose ok'))
+//     .catch(() => console.log('mongoose error'));
+//
+// app.use((req, res, next) => {
+//   const error = new Error('Not found');
+//   error.status = 404;
+//   next(error);
+// });
+//
+// app.use((error, req, res, next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   })
+// });
 
 module.exports = app;
