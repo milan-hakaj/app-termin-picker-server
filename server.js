4b4791f9e56e41338e82e6ff70cf94e051286c7a
milanhakaj <milan.hakaj@computerrock.com>
2019-01-19 17:34:28 +0100

    mx

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT ||  3000
 
 const requestHandler = (request, response) => {
   console.log(request.url)
-  response.end('Hello Node.js Server!')
+  response.end('Hello from sporty api!')
 }
 
 const server = http.createServer(requestHandler)
