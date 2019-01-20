7861c73536d01baf1c435fa32a72fb13139b26ae
milanhakaj <milan.hakaj@computerrock.com>
2019-01-20 18:06:47 +0100

    blahblah

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routesCities = require('./api/routes/cities');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World 2'))
+app.get('/', (req, res) => res.send('Hello World 3'))
 
 /* TODO use morgan only if environment is dev :) */
 app.use(morgan('dev'));
