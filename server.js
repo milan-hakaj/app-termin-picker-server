65782e3566e3108984e9ba674722575119394eb0
milanhakaj <milan.hakaj@computerrock.com>
2019-06-29 14:49:23 +0200

    socket ok

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const io = require('socket.io')(httpServer);
 
 httpServer.listen(3001);
 
-const DM = '[DM]';
-const DM_DETECTED = `${DM} ON DETECTED`;
-const DM_SEND = `${DM} SEND`;
-const DM_RECEIVE = `${DM} RECEIVE`;
-const DM_NEW = `${DM} NEW`;
-
 io.on('connection', function(socket){
   console.log('a user connected1', socket.id);
-  socket.on(DM_SEND, function(messageData){
+
+  socket.on('direct message sent', function(messageData){
     console.log('a user sent message2 -> ', messageData);
-    socket.broadcast.emit(DM_NEW, messageData);
+    socket.broadcast.emit('broadcast new message', messageData);
   });
 });
 
