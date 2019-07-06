c342c458003bd7284a5af4c8cff4fc2ead5e8f8c
milanhakaj <milan.hakaj@computerrock.com>
2019-07-06 15:16:07 +0200

    dmreceived

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const io = require('socket.io')(httpServer);
 
 httpServer.listen(3001);
 
+const sockets = {};
+
 io.on('connection', function(socket){
-  socket.on('direct message sent', function(messageData){
-    socket.broadcast.emit('broadcast new message', messageData);
+  const handshakeData = socket.request;
+  const userId = handshakeData._query['UserId'];
+
+  if (!sockets[userId]) {
+    sockets[userId] = socket.id;
+  }
+
+  socket.on('direct message sent', function(data){
+    const otherSocket = sockets[data.messageData.receiverId];
+    socket.to(otherSocket).emit('DIRECT_MESSAGE_RECEIVED', data);
   });
+
+
 });
 
