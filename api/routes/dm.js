dbeb28be5af0b3104418a0d0e3e657190e6521c0
milanhakaj <milan.hakaj@computerrock.com>
2019-07-09 15:18:21 +0200

    msg from to

--- api/routes/dm.js
+++ api/routes/dm.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { DM } = require('../models/DM');
 const mongoose = require('mongoose');
+const NUMBER_OF_DM_PER_REQUEST = 1;
 
 router.post('/', (req, res, next) => {
   const { receiverId, senderId, DMID } = req.body;
@@ -34,12 +35,27 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/:id', (req, res, next) => {
+  console.log('calin');
   DM.find({ _id : { $regex: req.params.id } }, { messages: { $slice: -10 } })
-    // .limit(1)
+    .limit(NUMBER_OF_DM_PER_REQUEST)
     .populate([{
         path: 'participants'
       }, {
         path: 'messages',
+    }])
+    .exec()
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(500).json({ error }));
+});
+
+router.post('/next/:id', (req, res, next) => {
+  DM.find({ _id : { $regex: req.params.id } }, { messages: { $slice: -10 } })
+    .skip(req.body.from)
+    .limit(req.body.to)
+    .populate([{
+      path: 'participants'
+    }, {
+      path: 'messages',
     }])
     .exec()
     .then(response => res.status(200).json(response))
